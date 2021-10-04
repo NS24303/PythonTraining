ebf531ed7b6f638949da1300677232f17d75e539
Nick <nstobbart@protonmail.com>
2021-10-04 19:51:47 +0100

    Initial upload of my testing script

---
+++ initial_testing.py
@@ -0,0 +1,30 @@
+x = 10
+y = 50
+z = 1.05
+
+T = True
+F = False
+
+welcome = "welcome world"
+
+# print the value of x
+print (x)
+#print the type of variable that x is
+print (type(x))
+print (type (z))
+print (type (welcome))
+print (type (T))
+#multiple x by y
+print (x*y)
+print ("hello world")
+print (welcome)
+
+#convert a variable from one to other - Called Casting
+print (type(x))
+xfloat = float(x)
+print (type(xfloat))
+
+#multiply an string with a int
+
+answer = input ("type a word: ")
+print (answer*x)
