63a04bf50b7ebeb9a8b12f10fca6bc73f0611dfd
Nick <nstobbart@protonmail.com>
2021-10-05 23:04:26 +0100

    removed some of the brackets where they were not needed

--- initial_testing.py
+++ initial_testing.py
@@ -11,25 +11,25 @@ print ("hello world")
 print (welcome)
 
 # print the value of x
-print ("-- The value of the variable x is: ",(x))
+print ("-- The value of the variable x is: ",x)
 #print the type of variable that x is
 print ("-- The variable type of x is", (type(x)),'\n')
 
-print ("-- The value of the variable z is: ",(z))
+print ("-- The value of the variable z is: ",z)
 print ("-- The variable type of z is", (type(z)),'\n')
 
-print ("-- The value of the variable welcome is: ",(welcome))
+print ("-- The value of the variable welcome is: ",welcome)
 print ("-- The variable type of welcome is", (type(welcome)),'\n')
 
-print ("-- The value of the variable T is: ",(T))
+print ("-- The value of the variable T is: ",T)
 print ("-- The variable type of T is", (type(T)),'\n')
 
 #multiple x by y
-print ("Lets try some math! x=",(x), "and y=",(y))
+print ("Lets try some math! x=",x, "and y=",y)
 print ("So if we multiple the two we get" ,(x*y),'\n')
 
 #convert a variable from one to other - Called Casting
-print ("Remember the current type of x is" ,type(x),"and the value is",(x))
+print ("Remember the current type of x is" ,type(x),"and the value is",x)
 print ("lets convert it to a float")
 xfloat = float(x)
 print ("the type of x is now",(type(xfloat)), "and the value of x is",(xfloat),'\n\n')
