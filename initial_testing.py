e9bff143fedbe7bd76d54a46a956ebfe73c33aa1
Nick <nstobbart@protonmail.com>
2021-10-05 23:00:42 +0100

    Many updates to the script to explain to the user what it is doing at run time by displaying better output via the print statement.s

--- initial_testing.py
+++ initial_testing.py
@@ -4,29 +4,38 @@ z = 1.05
 
 T =  True
 F = False
-#adding a test variable to ensure Pycharm and Git works.
 
-test = test
 welcome = "welcome world"
 
-# print the value of x
-print (x)
-#print the type of variable that x is
-print (type(x))
-print (type (z))
-print (type (welcome))
-print (type (T))
-#multiple x by y
-print (x*y)
 print ("hello world")
 print (welcome)
 
+# print the value of x
+print ("-- The value of the variable x is: ",(x))
+#print the type of variable that x is
+print ("-- The variable type of x is", (type(x)),'\n')
+
+print ("-- The value of the variable z is: ",(z))
+print ("-- The variable type of z is", (type(z)),'\n')
+
+print ("-- The value of the variable welcome is: ",(welcome))
+print ("-- The variable type of welcome is", (type(welcome)),'\n')
+
+print ("-- The value of the variable T is: ",(T))
+print ("-- The variable type of T is", (type(T)),'\n')
+
+#multiple x by y
+print ("Lets try some math! x=",(x), "and y=",(y))
+print ("So if we multiple the two we get" ,(x*y),'\n')
+
 #convert a variable from one to other - Called Casting
-print (type(x))
+print ("Remember the current type of x is" ,type(x),"and the value is",(x))
+print ("lets convert it to a float")
 xfloat = float(x)
-print (type(xfloat))
+print ("the type of x is now",(type(xfloat)), "and the value of x is",(xfloat),'\n\n')
 
 #multiply an string with a int
 
+print ("this next part has no purpose other than to demo what python will do when you multiply and Int with a String")
 answer = input ("type a word: ")
 print (answer*x)
