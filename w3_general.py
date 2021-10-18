0e64b9e2b4d3f2e9a8fc6af6039b707fbbb93aa7
Nick <nstobbart@protonmail.com>
2021-10-18 13:22:10 +0100

    initial commit of w3 work

---
+++ w3_general.py
@@ -0,0 +1,9 @@
+variable1=100
+
+
+if variable1 == 200:
+    print("variable 1 does not equal 100")
+elif variable1 == 100:
+    print ("Variable1 equal to 100")
+else:
+    print("Variable1 is equal to something else")
