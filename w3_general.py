099126c25d8b2c75d8ea18bd1f2eecfadf22a70f
Nick <nstobbart@protonmail.com>
2021-10-18 13:56:19 +0100

    Update

--- w3_general.py
+++ w3_general.py
@@ -1,4 +1,4 @@
-variable1=100
+variable1 = 100
 
 
 if variable1 == 200:
@@ -7,3 +7,13 @@ elif variable1 == 100:
     print ("Variable1 equal to 100")
 else:
     print("Variable1 is equal to something else")
+
+# ternary operator
+my_val = 10
+a = 'Greater_then_2' if my_val > 2 else 'Less_then_2'
+print(a)
+
+# None is its own datatype in Python. It is always false
+# it is the default return value from all functions if not using anything else.
+T = None
+print(type(T))
