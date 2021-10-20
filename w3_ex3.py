af93c4b867c03908b02c2496f206f39242b140b1
Nick <nstobbart@protonmail.com>
2021-10-20 20:36:38 +0100

    exercise3 complete

--- w3_ex3.py
+++ w3_ex3.py
@@ -5,3 +5,17 @@ Save these two items into variables and print them to the screen. You should ext
 system name and port id from the lines (i.e. your variables should only have 'twb-sf-hpsw1' and '15').
 Break out of your loop once you have retrieved these two items.
 '''
+
+with open("show_lldp_neighbors.txt") as f:
+    show_lldp = f.read()
+
+for line in show_lldp.splitlines():
+    fields = line.split()
+    if 'Port' in line:
+        port_id = fields[2]
+        continue
+    elif 'System' in line:
+        sys_id = fields[2]
+        break
+
+print(sys_id, port_id)
