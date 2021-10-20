a6db7189d82aa5698d2db3cd5cf5f83a729d8c6d
Nick <nstobbart@protonmail.com>
2021-10-20 20:56:59 +0100

    Updated exercise with comments

--- w3_ex3.py
+++ w3_ex3.py
@@ -9,6 +9,8 @@ Break out of your loop once you have retrieved these two items.
 with open("show_lldp_neighbors.txt") as f:
     show_lldp = f.read()
 
+# my approach below
+
 for line in show_lldp.splitlines():
     fields = line.split()
     if 'Port' in line:
@@ -19,3 +21,24 @@ for line in show_lldp.splitlines():
         break
 
 print(sys_id, port_id)
+print("Port ID: {}".format(port_id))
+
+'''
+Accepted answer again used boolean for the breaking out of script when the two variables were true
+It also used the format method for the print statement. I used this as an example in line 24 to highlight the usage
+The code below highlights the accepted answer. Note the _, this seems to take the text after system name or pord id
+and store it in the variable 
+
+system_name, port_id = (None, None)
+for line in show_lldp.splitlines():
+    if "System Name: " in line:
+        _, sys_id = line.split("System Name: ")
+    elif "Port id: " in line:
+        _, port_id = line.split("Port id: ")
+
+    if port_id and system_name:
+        break
+        
+print("System Name: {}".format(system_name))
+print("Port ID: {}".format(port_id))
+'''
