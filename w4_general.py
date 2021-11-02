662a6fad0b5ef8ba6a709b09906f28527bcd1526
Nick <nstobbart@protonmail.com>
2021-11-02 22:08:21 +0000

    minor updates

--- w4_general.py
+++ w4_general.py
@@ -62,5 +62,7 @@ for key, value in net_device.items():
 # Sets
 # note duplicate is there on purpose to demo that Sets ignore duplicates
 ipam = {'1.1.1.1', '8.8.8.8', '8.8.4.4', '1.0.0.1', '1.1.1.2', '1.1.1.1'}
-print(type(ipam))
+ipam_two = {'149.112.112.112', '9.9.9.9', '208.67.222.222', '208.67.220.220', '1.1.1.2', '1.1.1.1'}
+print(type(ipam), type(ipam_two))
 print(ipam)
+print(ipam_two)
