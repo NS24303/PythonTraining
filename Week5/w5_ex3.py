839ad2ab466d933f6f11fe4acca1049cff59abd6
Nick <nstobbart@protonmail.com>
2021-11-16 22:59:53 +0000

    Initial commit of w5 ex3

--- Week5/w5_ex3.py
+++ Week5/w5_ex3.py
@@ -19,3 +19,20 @@ should be converted to:
 
 Write several test cases for your function and verify it is working properly.
 '''
+
+def mac_format(mac_addr):
+    mac_addr = mac_addr.upper()
+    mac_addr = mac_addr.replace(".", "")
+    mac_addr = mac_addr.replace(":", "")
+    new_mac = []
+    while len(mac_addr) > 0:
+        entry = mac_addr[:2]
+        mac_addr = mac_addr[2:]
+        new_mac.append(entry)
+    new_mac = ":".join(new_mac)
+    return new_mac
+
+print(mac_format('001c.c4bf.826a'))
+
+# above works for the formatting of a mac address in either 00:1c:c4 format, or 00.1c.c4 or 001c.c4
+# need to spend time on this to figure out padding of a mac to 2 digits
